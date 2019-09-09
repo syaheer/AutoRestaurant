9059be512377c96bc865c04f775d0365a425bdb3
Sazali, Syahir Amir <Syahir.Sazali@chamberlain.com>
2019-09-08 20:57:14 -0500

    Initial commit

---
+++ app/src/main/java/com/syahiramir/autorestaurant/MainActivity.java
@@ -0,0 +1,13 @@
+package com.syahiramir.autorestaurant;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
